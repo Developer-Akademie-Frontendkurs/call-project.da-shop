97d307a44e29971dca27e4034f01d1f10d5387a1
Oliver Jung <oljung@posteo.de>
2025-10-03 09:03:03 +0200

    feat: update project structure and add product data, enhance HTML pages, and improve styling - Replaced db.json content with product data including names, descriptions, categories, and prices. - Enhanced index.html with a navigation bar and product display section. - Added about.html and contact.html pages with navigation and content. - Improved script.js to fetch and render product data dynamically. - Updated styles.css with new styles for layout, cards, and buttons.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,33 @@
-console.log('script.js gefunden');
+let products = [];
 
-let meine_variable = 'test';
+async function _init() {
+    await getProducts();
+    renderProducts();
+}
+
+async function getProducts() {
+    const request = await fetch('http://localhost:3000/products');
+    products = await request.json();
+    console.log(products);
+}
+
+function renderProducts() {
+    const productsContainer = document.getElementById('products');
+    productsContainer.innerHTML = '';
+
+    for (let productIndex = 0; productIndex < products.length; productIndex++) {
+        const product = products[productIndex];
+
+        productsContainer.innerHTML += /*html*/ `
+        <div class="card w-96 bg-base-100 card-md shadow-sm">
+            <div class="card-body">
+                <h2 class="card-title">${product.product_name}</h2>
+                <p>${product.product_short_description}</p>
+                <div class="justify-end card-actions">
+                    <button class="btn btn-primary">${product.price} €</button>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+}
